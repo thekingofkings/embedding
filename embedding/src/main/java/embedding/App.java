37b2f2fe3f2336077950529da259faa16ac3ee2b
Hongjian <hongjian-wang@outlook.com>
2016-12-07 15:34:43 -0500

    setup Project file structure

---
+++ embedding/src/main/java/embedding/App.java
@@ -0,0 +1,13 @@
+package embedding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
