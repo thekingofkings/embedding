615b3c64a2e9452d8a3fac0d815b9bb21d4fc0db
Hongjian <hongjian-wang@outlook.com>
2016-12-13 12:21:41 -0500

    binary classification on POI/demo labels

--- python/binaryClassification.py
+++ python/binaryClassification.py
@@ -20,16 +20,33 @@ def classification_LINE_features():
     l = pickle.load(open("../miscs/crime-label"))
     F = LINEfeatures()
     
+    print "Crime"
     clf1 = tree.DecisionTreeClassifier()
     scores1 = cross_val_score(clf1, F, l, cv=10)
-    print scores1.mean(), scores1
+    print "DT", scores1.mean(), scores1.std()
     
     clf2 = svm.SVC()
     scores2 = cross_val_score(clf2, F, l, cv=10)
-    print scores2.mean(), scores2    
+    print "SVM", scores2.mean(), scores2.std()
+    
+    l1 = pickle.load(open("../miscs/poi-label"))
+    l2 = pickle.load(open("../miscs/demo-label"))
+    l = dict(l1, **l2)
+    for k in l:
+        print k
+        if sum(l[k]) == 77:
+            print "Only one class"
+        else:
+            scores1 = cross_val_score(clf1, F, l[k], cv=10)
+            print "DT", scores1.mean(), scores1.std()
+            scores2 = cross_val_score(clf2, F, l[k], cv=10)
+            print "SVM", scores2.mean(), scores2.std()
+    return l
     
 
 if __name__ == "__main__":
+    l = classification_LINE_features()
+    
     f = np.loadtxt("../miscs/taxiFlow.csv", delimiter=",")
     import nimfa
     nmf = nimfa.Nmf(f, rank=10, max_iter=30, update="divergence", objective="conn", conn_change=50)
